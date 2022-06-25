4c0e8fc807f8b0684955279fb22005bd8f5fa118
Bianca Torres <bnk13.01.97@gmail.com>
2022-06-24 19:56:26 -0500

    Script that ask me for my Name and my Age

---
+++ namenage.py
@@ -0,0 +1,6 @@
+# Programa que te dice que edad tuviste el año pasado y cuanto tendrás el próximo.
+print("¿Cuál es tu nombre?")
+nombre = input()
+print("¿Cuántos años tienes?")
+edad = int(input())
+print(f"{nombre} el año pasado tenias {edad - 1} años y el próximo año cumplirás {edad + 1} años")
